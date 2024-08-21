fbeb757296862393ef05028af1af923945aef2fb
taguchi kazuki <elfubolfelicidadsuprema02@gmail.com>
2024-08-22 03:47:22 +0900

    feat: uniqueちょっとだけ

---
+++ unique/main.go
@@ -0,0 +1,11 @@
+package main
+
+// unique package
+// https://pkg.go.dev/unique@master
+func main() {
+	// ものすごいでかい構造体を比較する時に使うかな？？
+
+	// メモリを節約できるっぽい(https://future-architect.github.io/articles/20240719a/)
+	// 特定のシナリオにおいては、パフォーマンスの最適化ができそう
+	// one := unique.Make[int](1)
+}
