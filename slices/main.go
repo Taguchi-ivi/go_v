a5bb41fa227fcc1bcaced3643f8a246ca585014f
taguchi kazuki <elfubolfelicidadsuprema02@gmail.com>
2024-08-22 03:47:07 +0900

    feat: slices周りちょっと追加

--- slices/main.go
+++ slices/main.go
@@ -2,17 +2,38 @@ package main
 
 import (
 	"fmt"
+	"maps"
+	"slices"
 )
 
 // go run slices/main.go
 // wiki: https://pkg.go.dev/slices@master
 
-func main() {
+// values
+func values() {
+
+	// valueのみ
 	names := []string{"Alice", "Bob", "Vera"}
-	// for v := range slices.Values(names) {
-	// 	fmt.Println(v)
-	// }
-	for i, v := range names {
-		fmt.Println(i, v)
+	for v := range slices.Values(names) {
+		fmt.Println(v)
 	}
+}
+
+// sorted, keys
+func sortedAndKeys() {
+	m := map[string]int{
+		"a": 1,
+		"d": 2,
+		"c": 3,
+		"b": 4,
+	}
+	sorted := slices.Sorted(maps.Keys(m))
+	for k, v := range sorted {
+		fmt.Println(k, v)
+	}
+}
+
+func main() {
+	values()
+	sortedAndKeys()
 }
